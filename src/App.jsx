28fd16c2655347d5ec35cf0d3511e6a133d2a8f5
Palchhin <palchhinparihar@gmail.com>
2025-05-22 10:47:22 +0530

    setup the CamEra project.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-gray-100 p-4">
+        <h1 className="text-2xl font-bold">Vintage Camera Showcase</h1>
+      </div>
+    </>
+  )
+}
+
+export default App;
